3e293a4e8d381de78898b19c820c7a9bbe06ba9d
Franco <franco.vitalematus@gmail.com>
2020-08-10 19:22:41 -0400

    React-Testing: Pruebas Archivo 5 - Funciones: toEqual

---
+++ src/tests/base/05-funciones.test.js
@@ -0,0 +1,33 @@
+import '@testing-library/jest-dom';
+
+import { getUser, getUsuarioActivo } from '../../base/05-funciones'
+
+describe('Pruebas en 05-funciones', () => {
+
+    test('getUser debe retornar un objeto ', () => {
+
+        const userTest = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+        }
+
+        const user = getUser();
+
+        expect(user).toEqual(userTest);
+
+    })
+
+    // Tarea: getUsuarioActivo debe retornar un objeto
+    test('getUsuarioActivo debe retornar un objeto ', () => {
+
+        const nombre = 'Juan';
+        const user = getUsuarioActivo(nombre);
+
+        expect(user).toEqual({
+            uid: 'ABC567',
+            username: nombre
+        });
+
+    })
+
+})
