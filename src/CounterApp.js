d1c32dd23ef508e64fe14a6787d6278d68da5b93
Franco <franco.vitalematus@gmail.com>
2020-08-09 21:14:23 -0400

    React: Evento click - Eventos Generales

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -4,10 +4,17 @@ import PropTypes from 'prop-types';
 // Funtional Components
 const CounterApp = ({ value }) => {
 
+    // handleAdd: Evento click
+    const handleAdd = (e) => {
+        console.log(e)
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
             <h2> {value} </h2>
+
+            <button onClick={handleAdd}>+1</button>
         </>
     );
 }
