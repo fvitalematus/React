998fe72ee1795567040713b9e2fedc96cb8a4f9f
Franco <franco.vitalematus@gmail.com>
2020-08-09 22:03:13 -0400

    React: UserState - Hook

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 // Funtional Components
 const CounterApp = ({ value }) => {
 
+    const [counter, setCounter] = useState(0); // []
+
     // handleAdd: Evento click
-    const handleAdd = (e) => {
-        console.log(e)
+    const handleAdd = () => {
+        setCounter(counter + 1);
+        // setCounter( (c) => c + 1);
     }
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> {value} </h2>
+            <h2> {counter} </h2>
 
             <button onClick={handleAdd}>+1</button>
         </>
