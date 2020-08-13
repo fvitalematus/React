d51297a06fdd71e070ce69c977aa6071638e6709
Franco <franco.vitalematus@gmail.com>
2020-08-12 21:35:15 -0400

    React-Testing: Pruebas Archivo 11 - Async-Await y Fetch

---
+++ src/tests/base/11-async-await.test.js
@@ -0,0 +1,16 @@
+import { getImagen } from '../../base/11-async-await';
+
+
+describe('Pruebas con async-await y Fetch', () => {
+
+    test('debe retornar el url de la imagen ', async () => {
+
+        const url = await getImagen();
+        //console.log(url)
+
+        expect(url.includes('https://')).toBe(true);
+
+    });
+
+
+})
