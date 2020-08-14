50bff28474d255e2d78d9322df2b8e043fd6cfa7
Franco <franco.vitalematus@gmail.com>
2020-08-13 23:28:19 -0400

    React-Testing: Pruebas Archivo CounterApp - Eventos - Click Reset

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -48,5 +48,19 @@ describe('Pruebas en el <CounterApp />', () => {
 
     });
 
+    test('debe colocar el valor por defecto con el boton de reset ', () => {
+
+        const wrapper = shallow(<CounterApp value={100} />);
+
+        wrapper.find('button').at(0).simulate('click');
+        wrapper.find('button').at(0).simulate('click');
+        wrapper.find('button').at(1).simulate('click');
+
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('100');
+
+    });
+
 
 });
