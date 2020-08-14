28f1bc693a47ef16fd666d60ebcae472f2041891
Franco <franco.vitalematus@gmail.com>
2020-08-13 22:57:37 -0400

    React-Testing: Pruebas Archivo CounterApp - Eventos - Click + y -

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -6,22 +6,47 @@ import CounterApp from '../CounterApp';
 
 describe('Pruebas en el <CounterApp />', () => {
 
+    let wrapper = shallow(<CounterApp />);
+
+    // Antes de todo, para volver inicializar las pruebas antes de sus evaluaciones
+    beforeEach(() => {
+
+        wrapper = shallow(<CounterApp />);
+
+    });
+
     test('debe mostrar <CounterApp /> correctamente', () => {
-        
-        const wrapper = shallow(<CounterApp />)
+
+        // Para renderizar el DOM se aplica el Snapshot
         expect(wrapper).toMatchSnapshot();
 
     });
 
     test('debe mostrar el valor por defecto de 100', () => {
 
-        const wrapper = shallow(<CounterApp value={100} /> );        
+        const wrapper = shallow(<CounterApp value={100} />);
 
         const counterText = wrapper.find('h2').text().trim();
 
         expect(counterText).toBe('100');
 
-    });    
+    });
+
+    test('debe incrementar con el boton +1', () => {
+
+        wrapper.find('button').at(0).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+        expect(counterText).toBe('1');
+
+    });
+
+    test('debe disminuir con el boton -1', () => {
+
+        wrapper.find('button').at(2).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+        expect(counterText).toBe('-1');
+
+    });
 
 
 });
